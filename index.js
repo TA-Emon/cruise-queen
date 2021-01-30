ffbb427f443fc7432837b9ca9fb3d9fbf59efaaf
TA-Emon <tanviremon726@gmail.com>
2021-01-30 22:15:55 +0600

    modified

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 var BookNow = document.getElementById("book-now").addEventListener("click",function(){
     var BookingFrom = document.getElementById("booking-form");
     BookingFrom.style.display= "none";
+    var BookingContent = document.getElementById("booking-content");
+    BookingContent.style.display= "none";
 
-    var BookingFrom = document.getElementById("confirmation-message");
-    BookingFrom.style.display= "block";
+    var message = document.getElementById("confirmation-message");
+    message.style.display= "block";
 });
 
 var increaseFristClass = document.getElementById("increase-frist-class").addEventListener('click',function(){
     ticketsNumberCounter(true, "first-class-tickets");
-    total();
+    sumOfAllTickets();
 })
 
 var increaseFristClass = document.getElementById("decrease-frist-class").addEventListener('click',function(){
     ticketsNumberCounter(false, "first-class-tickets");
-    total();
+    sumOfAllTickets();
 })
 
 var increaseFristClass = document.getElementById("increase-economy-class").addEventListener('click',function(){
     ticketsNumberCounter(true, "economy-class-tickets");
-    total();
+    sumOfAllTickets();
 })
 
 var increaseFristClass = document.getElementById("decrease-economy-class").addEventListener('click',function(){
     ticketsNumberCounter(false, "economy-class-tickets");
-    total();
+    sumOfAllTickets();
 })
 
 
-
+// the function below will find out the number of tickets.....
 function ticketsNumberCounter(isIncrese, tickets){
-    var firstClassTickets = document.getElementById(tickets).value;
-    var firstClass = parseInt(firstClassTickets);
+    var totalTickets = document.getElementById(tickets).value;
+    var firstClass = parseInt(totalTickets);
     var increaseTickets = firstClass;
     if(isIncrese == true){
         increaseTickets = increaseTickets+1;
@@ -42,16 +44,17 @@ function ticketsNumberCounter(isIncrese, tickets){
 }
 
 
-function total(){
-    var firstTicketAll = document.getElementById("first-class-tickets").value;
-    var firstTicketAllInt = parseInt(firstTicketAll);
+// the function below will sum all the tickets price....
+function sumOfAllTickets(){
+    var firstTickets = document.getElementById("first-class-tickets").value;
+    var totalFirstClass = parseInt(firstTickets);
 
-    var ecomonyTicketAll = document.getElementById("economy-class-tickets").value;
-    var ecomonyTicketAllInt = parseInt(ecomonyTicketAll);
+    var ecomonyTickets = document.getElementById("economy-class-tickets").value;
+    var totalEconomyClass = parseInt(ecomonyTickets);
 
-    var firstTotal = firstTicketAllInt*150;
-    var economyTotal = ecomonyTicketAllInt*100;
-    var subtotal = firstTotal + economyTotal;
+    var fristClassPrice = totalFirstClass*150;
+    var economyClassPrice = totalEconomyClass*100;
+    var subtotal = fristClassPrice + economyClassPrice;
     document.getElementById("subtotal").innerText=subtotal;
 
     var tax = subtotal*0.1;
